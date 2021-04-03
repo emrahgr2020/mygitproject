6dcc236934491dfec50b7c0a0377e47ac62f25be
Emrullah Celebi <cumali20182019@gmail.com>
2021-04-03 19:50:09 +0300

    versiyon 03

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -5,7 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Versiyon 1");
 		System.out.println("My second commit");
-
+		System.out.println("Git ile ilk ders bitti");
 	}
 
 }
