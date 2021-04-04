f0c32e645cc2a37a2b5828620b4a765f42cc59b8
Emrullah Celebi <78147110+emrahgr2020@users.noreply.github.com>
2021-04-04 19:03:18 +0300

    Update MyGitClass.java

--- src/mygitprackage/MyGitClass.java
+++ src/mygitprackage/MyGitClass.java
@@ -8,6 +8,7 @@ public class MyGitClass {
 		System.out.println("Git ile ilk ders bitti");
 		System.out.println("Git ile ilk ders bitti2");
 		System.out.println("Uzak sunucudaki kodum");//github .com daki yeni kodum
+		System.out.println("Takim arkadasimin kodu");
 	}
 
 }
